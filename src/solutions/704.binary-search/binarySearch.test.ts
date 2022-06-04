555999d163cbbdbb238b29d7653ea19fb625304a
Edward <edwardrockrw@gmail.com>
2022-06-05 01:00:32 +0800

    binary search - 2

---
+++ src/solutions/704.binary-search/binarySearch.test.ts
@@ -0,0 +1,10 @@
+import binarySearch from "./index";
+
+describe("binarySearch" , ()=>{
+    it("input:[-1,0,3,5,9,12] , target:9 , output:4" , ()=>{
+        expect(binarySearch([-1,0,3,5,9,12],9)).toBe(4);
+    })
+    it("input:[-1,0,3,5,9,12] , target:2 , output:-1" , ()=>{
+        expect(binarySearch([-1,0,3,5,9,12],2)).toBe(-1);
+    })
+});
---
+++ src/solutions/704.binary-search/index.ts
@@ -0,0 +1,22 @@
+function binarySearch(nums: number[] , target: number): number{
+    let left: number = 0;
+    let right: number = nums.length;
+    
+    while (left <= right) {
+        let mid:number = Math.floor(left + (right - left)/2 );
+        
+        if(nums[mid] === target){
+            return mid;
+        }
+        else if(nums[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    
+    return -1;
+}
+
+export default binarySearch;
